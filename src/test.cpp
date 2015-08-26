64bfa65cc4d9a2210280b6a5921f579443cce5df
OliveiraDan <oliveiradandev@gmail.com>
2015-08-25 19:01:39 -0600

    Aug25, 7:01pm

--- src/test.cpp
+++ src/test.cpp
@@ -7,6 +7,7 @@ int main()
   std::cout << "Hello World!" << std::endl;
   std::cout << "Testing1...." << std::endl;
   std::cout << "Testing2..."  << std::endl;
+  std::cout << "Dronebuild2..." << std::endl; 
   return 0;
 }
 
